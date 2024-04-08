98cf51bce702935ee91595c277bd497e58aaadde
laboratorio <laboratorio@UPEAULA-31723>
2024-04-08 10:43:37 -0500

    Agregar archivos de proyecto.

---
+++ Web_Aplicacion_Ruiz_Jefferson/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Aplicacion_Ruiz_Jefferson.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
